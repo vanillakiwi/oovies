78a449aa07c5df06f98af86c93ec5a484b4b5c32
Rainie Zhao <rainiezhao@Rainies-MacBook-Pro-2.local>
2023-03-14 14:56:42 -0700

    finished Rating & Reviews model and DAO, add test cases in Inserter

---
+++ src/main/java/oovies/model/Rating.java
@@ -0,0 +1,62 @@
+package oovies.model;
+
+import java.util.Date;
+
+/**
+ * Rating is a simple, plain old java objects (POJO).
+ */
+public class Rating {
+	protected int ratingId;
+	protected double score;
+	protected Person user;
+	protected Movie movie;
+	
+	public Rating(int ratingId, double score, Person user, Movie movie) {
+		this.ratingId = ratingId;
+		this.score = score;
+		this.user = user;
+		this.movie = movie;
+	}
+	
+	public Rating(int ratingId) {
+		this.ratingId = ratingId;
+	}
+	
+	public Rating(double score, Person user, Movie movie) {
+		this.score = score;
+		this.user = user;
+		this.movie = movie;
+	}
+
+	public int getRatingId() {
+		return ratingId;
+	}
+
+	public void setRatingId(int ratingId) {
+		this.ratingId = ratingId;
+	}
+
+	public double getScore() {
+		return score;
+	}
+
+	public void setScore(double score) {
+		this.score = score;
+	}
+
+	public Person getUser() {
+		return user;
+	}
+
+	public void setUser(Person user) {
+		this.user = user;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+}
--- src/main/java/oovies/tools/Inserter.java
+++ src/main/java/oovies/tools/Inserter.java
@@ -2,9 +2,12 @@ package oovies.tools;
 
 import java.sql.SQLException;
 import java.text.ParseException;
+import java.util.Date;
+import java.util.List;
 
 import oovies.dal.*;
 import oovies.model.*;
+import reviewapp.model.Reviews;
 
 
 
@@ -14,6 +17,85 @@ import oovies.model.*;
 public class Inserter {
 
 	public static void main(String[] args) throws SQLException, ParseException {
+		ReviewsDao reviewsDao = ReviewsDao.getInstance();
+		RatingDao ratingDao = RatingDao.getInstance();
 		
+		// Reviews
+		// Create reviews
+		Date date = new Date();
+		Reviews review1 = new Reviews(date, "Great movie", user1, movie1);
+		review1 = reviewsDao.create(review1);
+		Reviews review2 = new Reviews(date, "Superb!", user1, movie2);
+		review2 = reviewsDao.create(review2);
+		Reviews review3 = new Reviews(date, "Not good", user4, movie3);
+		review3 = reviewsDao.create(review3);
+		Reviews review4 = new Reviews(date, "Not good at all", user4, movie1);
+		review4 = reviewsDao.create(review4);
+		
+		// Get review by reviewId
+		Reviews reviewOne = reviewsDao.getReviewById(1);
+		System.out.format("Reading reviewOne: reviewId:%d content:%s userId:%d movieId:%d\n", 
+				reviewOne.getReviewId(), reviewOne.getContent(), 
+				reviewOne.getPerson().getUserId(),
+				reviewOne.getMovie().gerMovieId());
+		
+		// Get review list by given UserId
+		List<Reviews> rUserList = reviewsDao.getReviewsByUserId(1);
+		for (Reviews r : rUserList) {
+			System.out.format("Looping reviews created by UserId 1: reviewId:%d "
+					+ "content:%s\n", r.getReviewId(), r.getContent());
+			}
+				
+		List<Reviews> rMovieList = reviewsDao.getReviewsByMovieId(1);
+			for (Reviews r : rMovieList) {
+				System.out.format("Looping reviews for movie1: reviewId:%db"
+						+ "content:%s userId:%d\n", r.getReviewId(), r.getContent(),
+						r.getPerson().getUserId());
+			}
+				
+		System.out.println("Deleting review2 ...");
+		Reviews review2Deleted = reviewsDao.delete(review2);
+		if (review2Deleted == null) {
+			System.out.println("review2 has been successfully deleted.");
+		}
+		
+		// Rating
+		// Create ratings
+		Rating rating1 = new Rating(10.0, user1, movie1);
+		rating1 = ratingsDao.create(rating1);
+		Rating rating2 = new Rating(9.5, user1, movie2);
+		rating2 = ratingsDao.create(rating2);
+		Rating rating3 = new Rating(5.0, user4, movie3);
+		rating3 = ratingsDao.create(rating3);
+		Rating rating4 = new Rating(1.0, user4, movie1);
+		rating4 = ratingsDao.create(rating4);
+				
+		// Get rating by ratingId
+		Rating ratingOne = ratingsDao.getRatingById(1);
+		System.out.format("Reading ratingOne: ratingId:%d score:%d userId:%d movieId:%d\n", 
+		ratingOne.getRatingId(), ratingOne.getScore(), 
+		ratingOne.getPerson().getUserId(),
+		ratingOne.getMovie().gerMovieId());
+				
+		// Get rating list by given UserId
+		List<Rating> rUserList = ratingsDao.getRatingByUserId(1);
+		for (Rating r : rUserList) {
+		System.out.format("Looping ratings created by UserId 1: ratingId:%d "
+				+ "score:%d movieId:%d\n", 
+				r.getRatingId(), r.getScore(), r.getMovie().getMovieId());
+		}
+						
+		List<Rating> rMovieList = ratingsDao.getRatingByMovieId(1);
+		for (Rating r : rMovieList) {
+			System.out.format("Looping ratings for movie1: ratingId:%db"
+				+ "score:%d userId:%d\n", r.getRatingId(), r.getScore(),
+				r.getPerson().getUserId());
+		}
+						
+		System.out.println("Deleting rating2 ...");
+		Rating rating2Deleted = ratingsDao.delete(rating2);
+		if (rating2Deleted == null) {
+			System.out.println("rating2 has been successfully deleted.");
+		}
 	}
 }
