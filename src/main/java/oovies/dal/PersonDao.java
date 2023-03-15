0b1969aabc20191d91ace99d3039300207f9628d
Rainie Zhao <rainiezhao@Rainies-MacBook-Pro-2.local>
2023-03-14 23:27:05 -0700

    revise ReviewsDao & RatingDao

---
+++ src/main/java/oovies/dal/PersonDao.java
@@ -0,0 +1,245 @@
+package oovies.dal;
+
+import oovies.model.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class PersonDao {
+    protected ConnectionManager connectionManager;
+
+    // Single pattern: instantiation is limited to one object.
+    private static PersonDao instance = null;
+
+    protected PersonDao() {
+        connectionManager = new ConnectionManager();
+    }
+
+    public static PersonDao getInstance() {
+        if (instance == null) {
+            instance = new PersonDao();
+        }
+        return instance;
+    }
+
+    /**
+     * Create the Person instance.
+     * This runs a INSERT statement.
+     */
+    public Person create(Person person) throws SQLException {
+        String insertPerson = "INSERT INTO Person(UserName, Password, Email, Role) VALUES(?,?,?,?);";
+        Connection connection = null;
+        PreparedStatement insertStmt = null;
+        try {
+            connection = connectionManager.getConnection();
+            insertStmt = connection.prepareStatement(insertPerson);
+            insertStmt.setString(1, person.getUserName());
+            insertStmt.setString(2, person.getPassword());
+            insertStmt.setString(3, person.getEmail());
+            insertStmt.setString(4, person.getRole().name());
+
+            insertStmt.executeUpdate();
+
+
+            return person;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+            if (insertStmt != null) {
+                insertStmt.close();
+            }
+        }
+    }
+
+
+    /**
+     * Delete the Person instance.
+     * This runs a DELETE statement.
+     */
+    public Person delete(Person person) throws SQLException {
+        String deletePerson = "DELETE FROM Person WHERE UserName=?;";
+        Connection connection = null;
+        PreparedStatement deleteStmt = null;
+        try {
+            connection = connectionManager.getConnection();
+            deleteStmt = connection.prepareStatement(deletePerson);
+            deleteStmt.setString(1, person.getUserName());
+            deleteStmt.executeUpdate();
+
+            // Return null so the caller can no longer operate on the Person instance.
+            return null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+            if (deleteStmt != null) {
+                deleteStmt.close();
+            }
+        }
+    }
+
+    /**
+     * Get the Person record by fetching it from your MySQL instance.
+     * This runs a SELECT statement and returns a single Person instance.
+     */
+    public Person getPersonByUserName(String userName) throws SQLException {
+        String selectPerson = "SELECT UserId, UserName, Password, Email, Role FROM Person WHERE UserName=?;";
+        Connection connection = null;
+        PreparedStatement selectStmt = null;
+        ResultSet results = null;
+        try {
+            connection = connectionManager.getConnection();
+            selectStmt = connection.prepareStatement(selectPerson);
+            selectStmt.setString(1, userName);
+            results = selectStmt.executeQuery();
+            if (results.next()) {
+                int userId = results.getInt("UserId");
+                String resultPersonName = results.getString("UserName");
+                String password = results.getString("Password");
+                String email = results.getString("Email");
+                String role = results.getString("Role");
+                Person person = new Person(userId, resultPersonName, password, email, Person.Role.valueOf(role));
+                return person;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+            if (selectStmt != null) {
+                selectStmt.close();
+            }
+            if (results != null) {
+                results.close();
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * Get the Person record by fetching it from your MySQL instance.
+     * This runs a SELECT statement and returns a single Person instance.
+     */
+    public Person getPersonByUserId(int userId) throws SQLException {
+        String selectPerson = "SELECT UserId, UserName, Password, Email, Role FROM Person WHERE UserId=?;";
+        Connection connection = null;
+        PreparedStatement selectStmt = null;
+        ResultSet results = null;
+        try {
+            connection = connectionManager.getConnection();
+            selectStmt = connection.prepareStatement(selectPerson);
+            selectStmt.setInt(1, userId);
+            results = selectStmt.executeQuery();
+            if (results.next()) {
+                int userId1 = results.getInt("UserId");
+                String resultPersonName = results.getString("UserName");
+                String password = results.getString("Password");
+                String email = results.getString("Email");
+                String role = results.getString("Role");
+                Person person = new Person(userId, resultPersonName, password, email, Person.Role.valueOf(role));
+                return person;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+            if (selectStmt != null) {
+                selectStmt.close();
+            }
+            if (results != null) {
+                results.close();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Get the UserId record by fetching it from your MySQL instance.
+     * This runs a SELECT statement and returns a single Person instance.
+     */
+    public int getUserIdByUserName(String userName) throws SQLException {
+        return getPersonByUserName(userName).getUserId();
+
+    }
+
+
+    /**
+     * Update the Password of the Person instance.
+     * This runs a UPDATE statement.
+     */
+    public Person updatePassword(Person person, String password) throws SQLException {
+        String updatePerson = "UPDATE Person SET Password=? WHERE UserName=?;";
+        Connection connection = null;
+        PreparedStatement updateStmt = null;
+        try {
+            connection = connectionManager.getConnection();
+            updateStmt = connection.prepareStatement(updatePerson);
+            updateStmt.setString(1, password);
+            updateStmt.setString(2, person.getUserName());
+            updateStmt.executeUpdate();
+
+
+            person.setPassword(password);
+            return person;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+            if (updateStmt != null) {
+                updateStmt.close();
+            }
+        }
+    }
+
+
+    /**
+     * Update the Email of the Person instance.
+     * This runs a UPDATE statement.
+     */
+    public Person updateEmail(Person person, String email) throws SQLException {
+        String updatePerson = "UPDATE Person SET Email=? WHERE UserName=?;";
+        Connection connection = null;
+        PreparedStatement updateStmt = null;
+        try {
+            connection = connectionManager.getConnection();
+            updateStmt = connection.prepareStatement(updatePerson);
+            updateStmt.setString(1, email);
+            updateStmt.setString(2, person.getUserName());
+            updateStmt.executeUpdate();
+
+
+            person.setEmail(email);
+            return person;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+            if (updateStmt != null) {
+                updateStmt.close();
+            }
+        }
+    }
+
+}
--- src/main/java/oovies/tools/Inserter.java
+++ src/main/java/oovies/tools/Inserter.java
@@ -17,42 +17,50 @@ import reviewapp.model.Reviews;
 public class Inserter {
 
 	public static void main(String[] args) throws SQLException, ParseException {
+		PersonDao personDao = PersonDao.getInstance();
 		ReviewsDao reviewsDao = ReviewsDao.getInstance();
 		RatingDao ratingDao = RatingDao.getInstance();
+		
+		Person person = new Person("user", "password", "email", Person.Role.ADMIN);
+		Person person1 = new Person("user1", "password1", "email1", Person.Role.USER);
+		personDao.create(person);
+		personDao.create(person1);
 		
 		// Reviews
 		// Create reviews
 		Date date = new Date();
-		Reviews review1 = new Reviews(date, "Great movie", user1, movie1);
+		Reviews review1 = new Reviews(date, "Great movie", person, movie1);
 		review1 = reviewsDao.create(review1);
-		Reviews review2 = new Reviews(date, "Superb!", user1, movie2);
+		Reviews review2 = new Reviews(date, "Superb!", person, movie2);
 		review2 = reviewsDao.create(review2);
-		Reviews review3 = new Reviews(date, "Not good", user2, movie3);
+		Reviews review3 = new Reviews(date, "Not good", person1, movie1);
 		review3 = reviewsDao.create(review3);
-		Reviews review4 = new Reviews(date, "Not good at all", user2, movie1);
+		Reviews review4 = new Reviews(date, "Not good at all", person1, movie2);
 		review4 = reviewsDao.create(review4);
 		
 		// Get review by reviewId
 		Reviews reviewOne = reviewsDao.getReviewById(1);
-		System.out.format("Reading reviewOne: reviewId:%d content:%s userId:%d movieId:%d\n", 
-				reviewOne.getReviewId(), reviewOne.getContent(), 
+		System.out.format("Reading reviewOne: reviewId:%d created:%s content:%s userId:%d movieId:%d\n", 
+				reviewOne.getReviewId(), reviewOne.getCreated(), reviewOne.getContent(), 
 				reviewOne.getUser().getUserId(),
 				reviewOne.getMovie().gerMovieId());
 		
 		// Get review list by given UserId
-		List<Reviews> rUserList = reviewsDao.getReviewsByUserId(1);
-		for (Reviews r : rUserList) {
+		List<Reviews> rvUserList = reviewsDao.getReviewsByUserId(1);
+		for (Reviews r : rvUserList) {
 			System.out.format("Looping reviews created by UserId 1: reviewId:%d "
 					+ "content:%s\n", r.getReviewId(), r.getContent());
 			}
-				
-		List<Reviews> rMovieList = reviewsDao.getReviewsByMovieId(1);
-			for (Reviews r : rMovieList) {
+		
+		// Get review list by given MovieId
+		List<Reviews> rvMovieList = reviewsDao.getReviewsByMovieId(1);
+			for (Reviews r : rvMovieList) {
 				System.out.format("Looping reviews for movie1: reviewId:%db"
 						+ "content:%s userId:%d\n", r.getReviewId(), r.getContent(),
 						r.getUser().getUserId());
 			}
-				
+		
+		// Delete review
 		System.out.println("Deleting review2 ...");
 		Reviews review2Deleted = reviewsDao.delete(review2);
 		if (review2Deleted == null) {
@@ -61,13 +69,13 @@ public class Inserter {
 		
 		// Rating
 		// Create ratings
-		Rating rating1 = new Rating(10.0, user1, movie1);
+		Rating rating1 = new Rating(10.0, person, movie1);
 		rating1 = ratingDao.create(rating1);
-		Rating rating2 = new Rating(9.5, user1, movie2);
+		Rating rating2 = new Rating(9.5, person, movie2);
 		rating2 = ratingDao.create(rating2);
-		Rating rating3 = new Rating(5.0, user2, movie3);
+		Rating rating3 = new Rating(5.0, person1, movie1);
 		rating3 = ratingDao.create(rating3);
-		Rating rating4 = new Rating(1.0, user2, movie1);
+		Rating rating4 = new Rating(1.0, person1, movie2);
 		rating4 = ratingDao.create(rating4);
 				
 		// Get rating by ratingId
@@ -84,14 +92,16 @@ public class Inserter {
 				+ "score:%d movieId:%d\n", 
 				r.getRatingId(), r.getScore(), r.getMovie().getMovieId());
 		}
-						
+		
+		// Get rating list by given MovieId
 		List<Rating> rMovieList = ratingDao.getRatingByMovieId(1);
 		for (Rating r : rMovieList) {
 			System.out.format("Looping ratings for movie1: ratingId:%db"
 				+ "score:%d userId:%d\n", r.getRatingId(), r.getScore(),
 				r.getUser().getUserId());
 		}
-						
+		
+		// Delete rating
 		System.out.println("Deleting rating2 ...");
 		Rating rating2Deleted = ratingDao.delete(rating2);
 		if (rating2Deleted == null) {
