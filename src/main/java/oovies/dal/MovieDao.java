98e51e93a0f4d16bf27694ec22da049fa96a5643
Rainie Zhao <rainiezhao@Rainies-MacBook-Pro-2.local>
2023-04-12 14:27:10 -0700

    added getMovieCountByAdvancedSearch

--- src/main/java/oovies/dal/MovieDao.java
+++ src/main/java/oovies/dal/MovieDao.java
@@ -399,18 +399,26 @@ public class MovieDao {
 
 		    StringBuilder sb = new StringBuilder();
 		    sb.append("SELECT MovieId,Title,ReleaseDate,Rating,Duration,Summary,DirectorId,StudioId,Genre "
-		              + "FROM Movie WHERE 1=1");
+		            + "FROM Movie WHERE 1=1");
+
+		    List<Object> params = new ArrayList<>();
+
 		    if (title != null && !title.isEmpty()) {
 		        sb.append(" AND Title LIKE ?");
+		        params.add("%" + title + "%");
 		    }
 		    if (genre != null) {
 		        sb.append(" AND Genre = ?");
+		        params.add(genre.toString());
 		    }
 		    if (year > 1923) {
 		        sb.append(" AND YEAR(ReleaseDate) = ?");
+		        params.add(year);
 		    }
 		    if (rating > 0.0) {
 		        sb.append(" AND Rating BETWEEN ? AND ?");
+		        params.add(rating);
+		        params.add(rating + 0.9);
 		    }
 
 		    // add pagination parameters
@@ -418,29 +426,17 @@ public class MovieDao {
 		        offset = 0;
 		    }
 		    sb.append(" LIMIT ? OFFSET ?");
+		    params.add(limit);
+		    params.add(offset);
 
 		    try (Connection connection = connectionManager.getConnection();
 		         PreparedStatement selectStmt = connection.prepareStatement(sb.toString())) {
 
 		        // Set the parameters for the query
 		        int paramIndex = 1;
-		        if (title != null && !title.isEmpty()) {
-		            selectStmt.setString(paramIndex++, "%" + title + "%");
+		        for (Object param : params) {
+		            selectStmt.setObject(paramIndex++, param);
 		        }
-		        if (genre != null) {
-		            selectStmt.setString(paramIndex++, genre.toString());
-		        }
-		        if (year > 1923) {
-		            selectStmt.setInt(paramIndex++, year);
-		        }
-		        if (rating > 0.0) {
-		            selectStmt.setDouble(paramIndex++, rating);
-		            selectStmt.setDouble(paramIndex++, rating + 0.9);
-		        }
-
-		        // set pagination parameters
-		        selectStmt.setInt(paramIndex++, limit);
-		        selectStmt.setInt(paramIndex++, offset);
 
 		        try (ResultSet results = selectStmt.executeQuery()) {
 		            DirectorDao directorDao = DirectorDao.getInstance();
@@ -472,16 +468,15 @@ public class MovieDao {
 		    }
 		    return movies;
 		}
+
 		
 		/**
-		 * Advance search to get movies' size base on given filter: title, genre, year and rating
+		 * Advance search to get movies' count base on given filter: title, genre, year and rating
 		 */
-		public int getMovieSizeByAdvancedSearch(String title, Movie.Genre genre, int year, double rating, int pageSize) throws SQLException {
-		    
-
+		public int getMovieCountByAdvancedSearch(String title, Movie.Genre genre, int year, double rating) throws SQLException {
+		    int count = 0;
 		    StringBuilder sb = new StringBuilder();
-		    sb.append("SELECT COUNT(*) AS NUM "
-		              + "FROM Movie WHERE 1=1");
+		    sb.append("SELECT COUNT(*) as count FROM Movie WHERE 1=1");
 		    if (title != null && !title.isEmpty()) {
 		        sb.append(" AND Title LIKE ?");
 		    }
@@ -497,8 +492,6 @@ public class MovieDao {
 
 		    try (Connection connection = connectionManager.getConnection();
 		         PreparedStatement selectStmt = connection.prepareStatement(sb.toString())) {
-
-		        // Set the parameters for the query
 		        int paramIndex = 1;
 		        if (title != null && !title.isEmpty()) {
 		            selectStmt.setString(paramIndex++, "%" + title + "%");
@@ -515,15 +508,15 @@ public class MovieDao {
 		        }
 
 		        try (ResultSet results = selectStmt.executeQuery()) {
-		            	if (results.next()) {
-			            	int num = (results.getInt("NUM") + pageSize - 1)/pageSize;
-			            	return num;
-			            }
+		            if (results.next()) {
+		                count = results.getInt("count");
+		            }
 		        }
 		    } catch (SQLException e) {
 		        e.printStackTrace();
 		        throw e;
 		    }
-		    return 0;
+		    return count;
 		}
+		
 }
--- src/main/java/oovies/servlet/FindMovies.java
+++ src/main/java/oovies/servlet/FindMovies.java
@@ -41,12 +41,12 @@ public class FindMovies extends HttpServlet {
 
 	    int page = 1;
 	    int moviesPerPage = 20;
-	    int offset = (page - 1) * moviesPerPage;
 
 	    if (pageStr != null && !pageStr.isEmpty()) {
 	        page = Integer.parseInt(pageStr);
 	    }
-	    offset = (page - 1) * moviesPerPage;
+
+	    int offset = (page - 1) * moviesPerPage;
 
 	    Movie.Genre genre = null;
 	    if (genreStr != null && !genreStr.isEmpty()) {
@@ -64,12 +64,16 @@ public class FindMovies extends HttpServlet {
 	    }
 
 	    try {
-	        movies = movieDao.getMovieByAdvancedSearch(title, genre, year, rating, offset, moviesPerPage);
-	        int totalMovies = movieDao.getMovieSizeByAdvancedSearch(title, genre, year, rating, moviesPerPage);
-	        int maxPage = (int) Math.ceil((double) totalMovies / moviesPerPage);
+	        // Get the list of movies and the total count
+	        List<Movie> movieList = movieDao.getMovieByAdvancedSearch(title, genre, year, rating, offset, moviesPerPage);
+	        int totalCount = movieDao.getMovieCountByAdvancedSearch(title, genre, year, rating);
+
+	        // Calculate the max page based on the total count
+	        int maxPage = (int) Math.ceil((double) totalCount / moviesPerPage);
+
 	        messages.put("success", "Displaying results for Title: " + title + " Genre: " + genreStr + " Year: " + yearStr
 	                + " Rating: " + ratingStr);
-	        req.setAttribute("movies", movies);
+	        req.setAttribute("movies", movieList);
 	        req.setAttribute("maxPage", maxPage);
 	    } catch (SQLException e) {
 	        e.printStackTrace();
@@ -79,4 +83,5 @@ public class FindMovies extends HttpServlet {
 	    req.setAttribute("pageIndex", page);
 	    req.getRequestDispatcher("/FindMovies.jsp").forward(req, resp);
 	}
+
 }
